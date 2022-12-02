f8d133364b7e936273f7f1db86251e337d42945d
alex.gama <alex.gama@inseinc.com>
2022-12-02 10:15:21 +0000

    Commit day 1 solutions

---
+++ Day 1/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+var data = File.ReadAllLines("data.txt").ToList();
+
+var individualElfCalories = new List<int>() { 0 };
+
+// Part 1
+data.ForEach( line => {
+    if ( String.IsNullOrWhiteSpace(line) ) {
+        individualElfCalories.Add(0);
+    } else {
+        int number = Int32.Parse(line);
+        individualElfCalories[individualElfCalories.Count - 1] += number;
+    }
+} ); 
+Console.WriteLine( "Part 1: " + individualElfCalories.Max());  
+
+// Part 2
+individualElfCalories.Sort( ( a, b ) => b - a );
+var top3Sum = individualElfCalories.Take(3).Sum();
+Console.WriteLine( "Part 2: " + top3Sum );
