b5f79d3c5b1cbada99bdc9f05d2923080ed2952c
alex.gama <alex.gama@inseinc.com>
2022-12-06 19:42:07 +0000

    Day 2 initial commit

---
+++ Day 2/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+enum OUTCOMES
+{
+    LOSE = 0,
+    DRAW = 3,
+    WIN = 6
+}
+
+enum ACTIONS 
+{
+    ROCK = 1,
+    PAPER = 2,
+    SCISSORS = 3
+}
