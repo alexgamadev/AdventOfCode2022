942a33c31759e5083ee40ae14875bcd3925fc3fb
alex.gama <alex.gama@inseinc.com>
2022-12-07 18:24:07 +0000

    Implement part 2 of day 4

--- Day 4/Program.cs
+++ Day 4/Program.cs
@@ -1,18 +1,34 @@
 ﻿
 var pairs = File.ReadAllLines( "input.txt" );
 
-int totalRedundantPairs = pairs.Select( pair =>
+// Part 1
+int totalOverlapPairs = pairs.Select( pair =>
 {
     String[] assignments = pair.Split( "," );
     int[] assignment1 = Array.ConvertAll(assignments[0].Split( "-" ), int.Parse);
     int[] assignment2 = Array.ConvertAll(assignments[1].Split( "-" ), int.Parse);
-    return IsOverlap(assignment1, assignment2) ? 1 : 0;
+    return IsFullOverlap(assignment1, assignment2) ? 1 : 0;
 } ).Sum();
 
-Console.WriteLine( totalRedundantPairs );
+// Part 2
+int anyOverlapPairs = pairs.Select( pair =>
+{
+    String[] assignments = pair.Split( "," );
+    int[] assignment1 = Array.ConvertAll(assignments[0].Split( "-" ), int.Parse);
+    int[] assignment2 = Array.ConvertAll(assignments[1].Split( "-" ), int.Parse);
+    return IsAnyOverlap(assignment1, assignment2) ? 1 : 0;
+} ).Sum();
 
-bool IsOverlap( int[] assignment1, int[] assignment2 )
+Console.WriteLine( totalOverlapPairs );
+Console.WriteLine( anyOverlapPairs );
+
+bool IsFullOverlap( int[] assignment1, int[] assignment2 )
 {
     return ( assignment1[0] >= assignment2[0] && assignment1[1] <= assignment2[1] 
     || assignment2[0] >= assignment1[0] && assignment2[1] <= assignment1[1]);
+}
+
+bool IsAnyOverlap( int[] assignment1, int[] assignment2 )
+{
+    return !( assignment1[1] < assignment2[0] || assignment2[1] < assignment1[0] );
 }
