39b3daaef54406c4db993b6ec50e6bc50fa19b60
ALEXGAMADEV\alexg <alexgama.97@outlook.com>
2022-12-07 16:47:42 +0000

    Add start of day 3 part 2

--- Day 3/Program.cs
+++ Day 3/Program.cs
@@ -1,18 +1,25 @@
 ﻿
 int prioritySum = 0;
 List<string> rucksacks = File.ReadAllLines("input.txt").ToList();
+
 rucksacks.ForEach( rucksack => 
 {
+    // Part 1
     // Wouldn't work for odd lengths but input guarantees even lengths
     string compartment1 = rucksack.Substring(0, rucksack.Length / 2);
     string compartment2 = rucksack.Substring(rucksack.Length / 2, rucksack.Length / 2);
 
     char sharedItem = compartment1.Intersect(compartment2).FirstOrDefault();
 
-    int priority = Char.IsLower( sharedItem ) ? (int)sharedItem - 96 : (int)sharedItem - 38;
+    int priority = GetItemPriority( sharedItem );
     prioritySum += priority;
 
     Console.WriteLine($"{sharedItem} with priority of {priority}");
 });
+
+int GetItemPriority( char item )
+{
+    return Char.IsLower( sharedItem ) ? (int)sharedItem - 96 : (int)sharedItem - 38;
+}
 
 Console.WriteLine( $"Final sum priority: {prioritySum}" );
