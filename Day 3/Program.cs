ebfc607a49ba1040042a388521ef06dda0f4569e
alex.gama <alex.gama@inseinc.com>
2022-12-07 17:56:01 +0000

    Clean up/improve solutions a bit

--- Day 3/Program.cs
+++ Day 3/Program.cs
@@ -1,46 +1,34 @@
 ﻿
 int prioritySum = 0;
 List<string> rucksacks = File.ReadAllLines("input.txt").ToList();
-int groupMemberIndex = 0;
 const int MAX_GROUP_MEMBERS = 3;
 int totalBadgePriority = 0;
-string groupPotentialBadges = String.Empty;
 
-rucksacks.ForEach( rucksack => 
+// Part 1 - Cleaner
+prioritySum = rucksacks.Select( rucksack => 
 {
-    // Part 1
     // Wouldn't work for odd lengths but input guarantees even lengths
     string compartment1 = rucksack.Substring(0, rucksack.Length / 2);
     string compartment2 = rucksack.Substring(rucksack.Length / 2, rucksack.Length / 2);
 
     char sharedItem = compartment1.Intersect(compartment2).FirstOrDefault();
 
-    int priority = GetItemPriority( sharedItem );
-    prioritySum += priority;
+    return GetItemPriority( sharedItem );
+}).Sum();
 
-    // Part 2
-    if ( groupMemberIndex == 0 ) 
+ // Part 2 - Handles any group sizes
+ totalBadgePriority = rucksacks.Chunk(MAX_GROUP_MEMBERS).Select( group => 
+ {
+    string potentialBadges = "";
+    potentialBadges = group[0];
+
+    for( int elf = 1; elf < MAX_GROUP_MEMBERS; elf++ )
     {
-        groupPotentialBadges = rucksack;
-        groupMemberIndex++;
-    } 
-    else
-    {
-        groupPotentialBadges = String.Join("", groupPotentialBadges.Intersect( rucksack ));
-        
-        if ( groupMemberIndex == MAX_GROUP_MEMBERS - 1 ) 
-        {
-            char badge = groupPotentialBadges.FirstOrDefault();
-            totalBadgePriority += GetItemPriority( badge );
-            groupPotentialBadges = String.Empty;
-            groupMemberIndex = 0;
-        } 
-        else 
-        {
-            groupMemberIndex++;
-        }
+        potentialBadges = String.Join( "", potentialBadges.Intersect(group[elf]) );
     }
-});
+
+    return GetItemPriority( potentialBadges.FirstOrDefault() );
+ } ).Sum();
 
 int GetItemPriority( char item )
 {
