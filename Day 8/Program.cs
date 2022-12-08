5d992aed0ed8c2c45924925f4cdeb5b33726aaa6
alex.gama <alex.gama@inseinc.com>
2022-12-08 23:16:07 +0000

    Implemented day 8 part 2

--- Day 8/Program.cs
+++ Day 8/Program.cs
@@ -3,7 +3,8 @@
 int TREE_COUNT_COLUMNS = data[0].Length;
 int TREE_COUNT_ROWS = data.Count;
 
-var totalTrees = data.Select( ( row, rowIndex ) => 
+// Part 1
+var totalVisibleTrees = data.Select( ( row, rowIndex ) => 
 {
     if( rowIndex == 0 || rowIndex == TREE_COUNT_ROWS - 1) 
     {
@@ -23,8 +24,29 @@ var totalTrees = data.Select( ( row, rowIndex ) =>
     }).Sum();
 } ).Sum();
 
-Console.WriteLine( totalTrees );
+Console.WriteLine( totalVisibleTrees );
 
+// Part 2
+var bestScenicScore = data.Select( ( row, rowIndex ) => 
+{
+    if( rowIndex == 0 || rowIndex == TREE_COUNT_ROWS - 1) 
+    {
+        return 0;
+    }
+    return row.Select( ( tree, colIndex) =>
+    {
+        if( colIndex == 0 || colIndex == TREE_COUNT_COLUMNS - 1 ) 
+        {
+            return 0;
+        } 
+        else
+        {
+            return GetScenicScore(data, rowIndex, colIndex);
+        }
+    }).Max();
+} ).Max();
+
+Console.WriteLine( bestScenicScore );
 
 // I know this is horribly inefficient but it works and I'm in a rush, will optimise when done
 bool GetVisible( List<string> data, int row, int col )
@@ -80,5 +102,57 @@ bool GetVisible( List<string> data, int row, int col )
     }
 
     return visibleDown || visibleLeft || visibleRight || visibleUp;
+}
+
+// I know this is horribly inefficient but it works and I'm in a rush, will optimise when done
+int GetScenicScore( List<string> data, int row, int col )
+{
+    int height = data[row][col];
+    int viewDistanceLeft = 0;
+    int viewDistanceUp = 0;
+    int viewDistanceDown = 0;
+    int viewDistanceRight = 0;
+
+    // Check left
+    for( int x = col - 1; x >= 0; x-- )
+    {
+        viewDistanceLeft++;
+        if( data[row][x] >= height )
+        {
+            break;
+        }
+    }
+
+    // Check right
+    for( int x = col + 1; x < TREE_COUNT_COLUMNS; x++ )
+    {
+        viewDistanceRight++;
+        if( data[row][x] >= height )
+        {
+            break;
+        }
+    }
+
+    // Check up
+    for( int y = row - 1; y >= 0; y-- )
+    {
+        viewDistanceUp++;
+        if( data[y][col] >= height )
+        {
+            break;
+        }
+    }
+
+    // Check down
+    for( int y = row + 1; y < TREE_COUNT_ROWS; y++ )
+    {
+        viewDistanceDown++;
+        if( data[y][col] >= height )
+        {
+            break;
+        }
+    }
+
+    return viewDistanceLeft * viewDistanceDown * viewDistanceRight * viewDistanceUp;
 }
 
